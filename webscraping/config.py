0a0c78de999c142ce246f08a264ab63b7aa25e94
Alvaro Torres F <33440322+AquaroTorres@users.noreply.github.com>
2019-11-25 12:18:43 -0300

    Delete config.py

--- webscraping/config.py
+++
@@ -1,4 +0,0 @@
-user = '102783875'
-password = 'Aa123456'
-driver = '/home/tic/urgency/webscraping/chromedriver_linux64/chromedriver'
-url = 'https://urgencia.saludenred.cl/Authentication/#/authentication'
