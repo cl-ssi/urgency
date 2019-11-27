e1d9ca983de6a7ca3b871da6c49c08ee7283c39b
AcuaroTorres <alvarotorres@gmail.com>
2019-11-27 10:23:30 -0300

    No se que cambió

--- webscraping/webscrap.py
+++ webscraping/webscrap.py
@@ -21,41 +21,42 @@ for nodo, idx in zip(config.nodos, config.ids):
     #medimos cuanto tiempo toma el scraping
     options = webdriver.ChromeOptions()
     options.add_argument('headless')
-    driver = webdriver.Chrome(config.driver,chrome_options=options)
+    #driver = webdriver.Chrome(config.driver,chrome_options=options)
+    driver = webdriver.Chrome(config.driver)
     driver.get(config.url)
-    
+
     ######################### INICIO DE LOGIN #########################
     # esperamos al elemento
     wait = WebDriverWait(driver, 90)
     username = wait.until(ec.visibility_of_element_located((By.NAME, "username")))
-    # con esto ya cargo el elemento 
+    # con esto ya cargo el elemento
     username.send_keys(config.user)
-    
+
     #encontramos el elemento password y enviamos la clave
     password_box = driver.find_element_by_id('password')
     password_box.send_keys(config.password)
-    
+
     #apretamos en el css
     driver.find_element_by_css_selector(".button").click()
     ############################ FIN DE LOGIN #########################
     end_time = time.time()
     print("Total ejecucion de login: {}".format(end_time - start_time))
-    
+
     ################ SE SELECCIONA EL NODO ##############################
     #xpath = "/html/body/div[2]/div/div[2]/div/ng-include/div/div/fielset/ul/li[{}]/form/div[3]/input".format(sys.argv[1])
     xpath = "/html/body/div[2]/div/div[2]/div/ng-include/div/div/fielset/ul/li[{}]/form/div[3]/input".format(nodo)
     seleccion = wait.until(ec.visibility_of_element_located((By.XPATH, xpath))).click()
-    
+
     end_time = time.time()
     print("Total ejecucion de seleccionar el nodo: {}".format(end_time - start_time))
-    
+
     xpath = "//*[@id='container']/div/div[2]/div[2]/p/span[1]"
     pacientes_espera = wait.until(ec.visibility_of_element_located((By.XPATH, xpath)))
-    
+
     numero = re.search('\(([^)]+)', pacientes_espera.text).group(1)
     print("Pacientes en espera: "+numero)
     driver.close()
-    
+
     #r = requests.post(config.webservice, data={'establishment_id': sys.argv[2], 'waiting': numero})
     r = requests.post(config.webservice, data={'establishment_id': idx, 'waiting': numero})
     print(r.status_code, r.reason)
@@ -63,4 +64,3 @@ for nodo, idx in zip(config.nodos, config.ids):
 # medimos cuanto finaliza el scraping
 end_time = time.time()
 print("Total ejecucion del webscraping: {}".format(end_time - start_time))
-    
